19b6b80416c2860a17dadeb756485f128c80dff2
Agustin <agustintorchia@gmail.com>
2019-06-05 10:54:29 -0300

    Creado Cliente Personas

---
+++ ClientePersonas/ClientePersonas/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientePersonas
+{
+    class Cliente
+    {
+        private List<Persona> personas;
+
+        internal List<Persona> Personas { get => personas; set => personas = value; }
+
+        public void agregarPersona(Persona persona)
+        {
+            personas.Add(persona);
+        }
+
+        public void recibirPersona(String persona)
+        {
+            String[] personas2;
+            personas2 = persona.Split('/');
+            agregarPersona(new Persona(personas2[0], personas2[1], personas2[2],Int32.Parse(personas2[3])));
+        }
+    }
+}
---
+++ ClientePersonas/ClientePersonas/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace ClientePersonas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConectarSV = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Apellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sexo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonRecibir = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonConectarSV
+            // 
+            this.buttonConectarSV.Location = new System.Drawing.Point(12, 12);
+            this.buttonConectarSV.Name = "buttonConectarSV";
+            this.buttonConectarSV.Size = new System.Drawing.Size(108, 23);
+            this.buttonConectarSV.TabIndex = 0;
+            this.buttonConectarSV.Text = "Conectar con el SV";
+            this.buttonConectarSV.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nombre,
+            this.Apellido,
+            this.Sexo});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 170);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(341, 150);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            // 
+            // Apellido
+            // 
+            this.Apellido.HeaderText = "Apellido";
+            this.Apellido.Name = "Apellido";
+            // 
+            // Sexo
+            // 
+            this.Sexo.HeaderText = "Sexo";
+            this.Sexo.Name = "Sexo";
+            // 
+            // buttonRecibir
+            // 
+            this.buttonRecibir.Location = new System.Drawing.Point(12, 41);
+            this.buttonRecibir.Name = "buttonRecibir";
+            this.buttonRecibir.Size = new System.Drawing.Size(108, 23);
+            this.buttonRecibir.TabIndex = 2;
+            this.buttonRecibir.Text = "Recibir Personas";
+            this.buttonRecibir.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 73);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.buttonRecibir);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonConectarSV);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConectarSV;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Apellido;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sexo;
+        private System.Windows.Forms.Button buttonRecibir;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
